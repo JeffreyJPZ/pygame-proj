ba8164130055192684bbb70e7f87ede8a55e1c75
Spitchimp <72636309+JeffreyJPZ@users.noreply.github.com>
2020-12-27 16:45:08 -0800

    Update Hotshot.py
    Changes to objects blits and formatting

--- Hotshot/Hotshot.py
+++ Hotshot/Hotshot.py
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 game_font = pygame.font.SysFont('comicsans', 40)
 
 # player car
-red_car = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'redf1car.png')), (win_width // 6, win_height // 4)).convert_alpha() # Transparent image
+red_car = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'redf1car.png')), (win_width // 6, win_height // 4)).convert_alpha()      # Transparent image
 
 # cars
 blue_car = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'bluef1car.png')), (win_width // 6, win_height // 4)).convert_alpha()
@@ -41,10 +41,10 @@ class Car:
         win.blit(self.car_img, (self.x, self.y))
 
     def car_width(self):
-        return self.car_img.get_width() # gets car width
+        return self.car_img.get_width()         # gets car width
 
     def car_height(self):
-        return self.car_img.get_height() # gets car height
+        return self.car_img.get_height()        # gets car height
 
 
 
@@ -52,29 +52,29 @@ class Player(Car):
     def __init__(self, x, y, health=100):
         super().__init__(x, y, health) 
         self.car_img = red_car
-        self.mask = pygame.mask.from_surface(self.car_img) # For "pixel-perfect" collision 
+        self.mask = pygame.mask.from_surface(self.car_img)      # For "pixel-perfect" collision 
         self.max_health = health
 
     def health_bar(self, win):
         pygame.draw.rect(win, (255,0,0), (self.x + self.car_img.get_width() + self.car_img.get_width() // 20, self.y, self.car_img.get_width() // 20, self.car_img.get_height())) # Coords, size of healthbar
         pygame.draw.rect(win, (0,255,0), (self.x + self.car_img.get_width() + self.car_img.get_width() // 20, self.y, self.car_img.get_width() // 20, self.car_img.get_height() * (self.health/self.max_health)))
 
-    def draw(self, win): # pickups change health bar
+    def draw(self, win):        # pickups change health bar
         super().draw(win)
         self.health_bar(win)
 
 
 
-class Enemy(Car):     #  Decreases health if collision, random spawn location, random color
+class Enemy(Car):           #  Decreases health if collision, random spawn location, random color
     
     enemy_cars = {"blue": blue_car, "green": green_car, "pink": pink_car, "purple": purple_car, "yellow": yellow_car}
     
     def __init__(self, x, y, colour, health=100):
         super().__init__(x, y, health)
         self.car_img = self.enemy_cars[colour]
-        self.mask = pygame.mask.from_surface(self.car_img) # collision detection of NPC car
+        self.mask = pygame.mask.from_surface(self.car_img)      # collision detection of NPC car
     
-    def enemy_move(self, game_speed): # Move cars left
+    def enemy_move(self, game_speed):       # Move cars left
         self.x -= game_speed
 
 
@@ -92,14 +92,14 @@ class Pickup:
         self.x -= game_speed
 
     def pickup_width(self):
-        return self.pickup_img.get_width() # gets pickup width
+        return self.pickup_img.get_width()      # gets pickup width
 
     def pickup_height(self):
-        return self.pickup_img.get_height() # gets pickup height
+        return self.pickup_img.get_height()     # gets pickup height
 
 
 
-class Wrench(Pickup):   # Fully repairs car, spawns less frequently
+class Wrench(Pickup):                   # Fully repairs car, spawns less frequently
     def __init__(self, x, y):
         super().__init__(x, y)
         self.pickup_img = wrench
@@ -107,7 +107,7 @@ class Wrench(Pickup):   # Fully repairs car, spawns less frequently
         
     
 
-class Tire(Pickup):     # Adds small amount of health, spawns more frequently
+class Tire(Pickup):                     # Adds small amount of health, spawns more frequently
     def __init__(self, x, y):
         super().__init__(x, y)
         self.pickup_img = tire
@@ -115,7 +115,7 @@ class Tire(Pickup):     # Adds small amount of health, spawns more frequently
 
     
 
-class Spike(Pickup):    # Instant game over if collision occurs
+class Spike(Pickup):                    # Instant game over if collision occurs
     def __init__(self, x, y):
         super().__init__(x, y)
         self.pickup_img = spike
@@ -123,11 +123,11 @@ class Spike(Pickup):    # Instant game over if collision occurs
         
 
 
-def collision(obj_a, obj_b):    # Check if point of intersection exists
-    offset_x = obj_b.x - obj_a.x # Dist. between x and y coords
+def collision(obj_a, obj_b):            # Check if point of intersection exists
+    offset_x = obj_b.x - obj_a.x        # Dist. between x and y coords
     offset_y = obj_b.y - obj_a.y
 
-    return obj_a.mask.overlap(obj_b.mask, (offset_x, offset_y)) != None # returns tuple and not "none"
+    return obj_a.mask.overlap(obj_b.mask, (offset_x, offset_y)) != None         # returns tuple and not "none"
 
     
 
@@ -137,25 +137,25 @@ def main():
     score = 0
     level = 0
 
-    # Many game parameters
-    scroll_speed = 10 # Change scroll speed of background, higher value for FAST
+    # Most game parameters
+    scroll_speed = 10                       # Change scroll speed of background, higher value for FAST
 
-    spawn_win_width = win_width * 5 # Greatest distance that objects can spawn
+    spawn_win_width = win_width * 5         # Greatest distance that objects can spawn
 
-    score_timer = 0 # Used for incrementing score
+    score_timer = 0                         # Used for incrementing score
 
     player = Player(0, win_height // 2)
     player_width = player.car_width()
     player_height = player.car_height()
 
-    lane_spawns = [0, win_height // 4, win_height // 2, win_height // 2 + win_height // 4] # Objects spawn on one of four lanes
+    lane_spawns = [0, win_height // 4, win_height // 2, win_height // 2 + win_height // 4]      # Objects spawn on one of four lanes
     
     enemy_cars = {"blue": blue_car, "green": green_car, "pink": pink_car, "purple": purple_car, "yellow": yellow_car}
     enemies = []
     enemy_num = 0
-    enemy_health_decrease = player.max_health // 4 # Change value as needed
+    enemy_health_decrease = player.max_health // 4      # Change value as needed
     
-    game_speed = 3 # Sets speed of objects
+    game_speed = 3                                      # Sets speed of objects
 
     wrenches = []
     tires = []
@@ -164,7 +164,7 @@ def main():
     wrench_num = 0
     tire_num = 0
     spike_num = 0
-    wrench_health_increase = player.max_health # Increment and decrement pickup values
+    wrench_health_increase = player.max_health          # Increment and decrement pickup values
     tire_health_increase = player.max_health // 4
     spike_health_decrease = player.max_health
     
@@ -176,13 +176,13 @@ def main():
     def backg_scroll():
         global backg_x1, backg_x2
         
-        if backg_x1 <= backg.get_width() * -1:  # Resets x coord of background 1
+        if backg_x1 <= backg.get_width() * -1:          # Resets x coord of background 1
             backg_x1 = backg.get_width()
 
-        if backg_x2 <= backg.get_width() * -1:  # Resets x coord of background 2 
+        if backg_x2 <= backg.get_width() * -1:          # Resets x coord of background 2 
             backg_x2 = backg.get_width()
 
-        backg_x1 -= scroll_speed # Both backgrounds move left
+        backg_x1 -= scroll_speed                        # Both backgrounds move left
         backg_x2 -= scroll_speed
 
 
@@ -194,8 +194,6 @@ def main():
         
         win.blit(backg, (backg_x1, 0))
         win.blit(backg, (backg_x2, 0))
-        win.blit(display_score, (win_width / 3 + win_width / 3 - display_score.get_width() / 2, win_height - display_score.get_height() - 20))
-        win.blit(display_level, (win_width / 3 - display_level.get_width() / 2, win_height - display_level.get_height() - 20))
         
         for enemy in enemies: # Draws objects to screen with attributes "draw" and "draw_pickup"
             enemy.draw(win)
@@ -208,6 +206,9 @@ def main():
 
         for spike in spikes:
             spike.draw_pickup(win)
+            
+        win.blit(display_score, (win_width / 3 + win_width / 3 - display_score.get_width() / 2, win_height - display_score.get_height() - 20)) # Score blitted after objects to prevent objects from covering the text
+        win.blit(display_level, (win_width / 3 - display_level.get_width() / 2, win_height - display_level.get_height() - 20))
 
         player.draw(win)
 
@@ -238,24 +239,24 @@ def main():
             game_over_pause += 1
 
         if game_over:
-            if game_over_pause > FPS * 5: # Wait x seconds before going to menu screen
+            if game_over_pause > FPS * 5:       # Wait x seconds before going to menu screen
                 run = False
             else:
                 continue
             
-        if score_timer % FPS == 0: # Every x seconds score goes up by y
+        if score_timer % FPS == 0:              # Every x seconds score goes up by y
             score += 1
 
-        if len(enemies) == 0 and len(wrenches) == 0 and len(tires) == 0 and len(spikes) == 0: # If all objects have been removed
+        if len(enemies) == 0 and len(wrenches) == 0 and len(tires) == 0 and len(spikes) == 0:       # If all objects have been removed
             level += 1 
-            enemy_num += 5 # Change enemy number as needed, 3-5 is fairly balanced
+            enemy_num += 5                      # Change enemy number as needed, 3-5 is fairly balanced
 
-            if level % 5 == 0: # Every 5 levels the game speed increases
+            if level % 5 == 0:                  # Every x levels the game and object speed increases
                 game_speed += 3
                 scroll_speed += 5
                 spawn_win_width *= 2
             
-            if random.randint(1, 100) <= 5:  # Chance of pickups spawning
+            if random.randint(1, 100) <= 5:     # Chance of pickups spawning
                 wrench_num += 1
 
             if random.randint(1, 100) <= 20:
@@ -264,8 +265,8 @@ def main():
             if random.randint(1, 100) <= 10:
                 spike_num += 1
             
-            for i in range(enemy_num):  # Spawns all objects at once
-                enemy = Enemy(random.randrange(win_width, spawn_win_width), random.choice(lane_spawns), random.choice(list(enemy_cars.keys()))) # Choose random colour
+            for i in range(enemy_num):          # Spawns all objects at once
+                enemy = Enemy(random.randrange(win_width, spawn_win_width), random.choice(lane_spawns), random.choice(list(enemy_cars.keys())))         # Choose random colour
                 enemies.append(enemy) # Add to list
                 
             for i in range(wrench_num):
@@ -286,12 +287,12 @@ def main():
               pygame.quit()
               sys.exit()
               
-            if event.type == pygame.KEYDOWN: # Single input
-                keys = pygame.key.get_pressed() # Check for input
-                if keys[pygame.K_w] or keys[pygame.K_UP]:    # move up one lane
+            if event.type == pygame.KEYDOWN:                    # Single input
+                keys = pygame.key.get_pressed()                 # Check for input
+                if keys[pygame.K_w] or keys[pygame.K_UP]:       # move up one lane
                     if player.y > 0:
                         player.y -= win_height // 4
-                if keys[pygame.K_s] or keys[pygame.K_DOWN]: # move down one lane
+                if keys[pygame.K_s] or keys[pygame.K_DOWN]:     # move down one lane
                     if player.y < win_height - player_height:
                         player.y += win_height // 4
 
@@ -299,17 +300,17 @@ def main():
             enemy.enemy_move(game_speed)
 
             if collision(enemy, player):
-                player.health -= enemy_health_decrease # subtract x health
-                enemies.remove(enemy) # Removes from list if collision occurs
+                player.health -= enemy_health_decrease      # subtract x health
+                enemies.remove(enemy)                       # Removes from list if collision occurs
                 
-            elif enemy.x + enemy.car_width() < 0: # Otherwise, deletes objects if off-screen
+            elif enemy.x + enemy.car_width() < 0:           # Otherwise, deletes objects if off-screen
                 enemies.remove(enemy)
 
         for wrench in wrenches[:]:
             wrench.pickup_move(game_speed)
 
             if collision(wrench, player):
-                player.health = wrench_health_increase # Sets health equal to max
+                player.health = wrench_health_increase      # Sets health equal to max
                 wrenches.remove(wrench)
                     
             elif wrench.x + wrench.pickup_width() < 0:
@@ -320,10 +321,10 @@ def main():
 
             if collision(tire, player):
                 
-                if player.health + tire_health_increase >= player.max_health: # If new health greater or equal to max health, health remains at max
+                if player.health + tire_health_increase >= player.max_health:       # If new health greater or equal to max health, health remains at max
                     player.health = player.max_health
                 else:
-                    player.health += tire_health_increase # Else health is increased by x
+                    player.health += tire_health_increase                           # Else health is increased by x
 
                 tires.remove(tire)
 
@@ -334,10 +335,11 @@ def main():
             spike.pickup_move(game_speed)
 
             if collision(spike, player):
-                player.health -= spike_health_decrease # Instant game over
+                player.health -= spike_health_decrease                              # Instant game over
                 
-                if player.health < 0: # Health cannot be negative
+                if player.health < 0:                                               # Health cannot be negative
                     player.health = 0
+                    
                 spikes.remove(spike)
 
             elif spike.x + spike.pickup_width() < 0:
@@ -373,6 +375,8 @@ def instructions():
     pygame.quit()
     sys.exit()
 
+    
+
 def menu_screen():
     title_font = pygame.font.SysFont('comicsans', 120, 1)
     menu_font = pygame.font.SysFont('comicsans', 60)
@@ -396,7 +400,7 @@ def menu_screen():
             if event.type == pygame.KEYDOWN:
                 keys = pygame.key.get_pressed()
                 
-                if keys[pygame.K_e]: # E pressed, goes to instruction screen
+                if keys[pygame.K_e]:                # E pressed, goes to instruction screen
                     instructions()
                 else:
                     main()
@@ -406,6 +410,7 @@ def menu_screen():
 
     pygame.quit()
     sys.exit()
+
 
 
 menu_screen()
